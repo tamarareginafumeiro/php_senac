0c80481575418641a1baf3ec63c25a98970b4d91
tamarareginafumeiro <tamara.rfumeiro@senacsp.edu.com>
2024-06-10 22:14:38 -0300

    aula 01 - manipulando strings

---
+++ 01/atividade.php
@@ -0,0 +1,12 @@
+<?php
+    # 1
+    // Crie uma variável para nome e outra para profissão
+    // Escreva a mensagem "Fulano é tal_profissao"
+
+    $nome = "Valdir";
+    $sobrenome = "Fernandes";
+    $profissao = "Soldador"
+    echo $nome . " " . $sobrenome . "  é " . $profissao;
+    echo "<br>";
+
+?>
--- 01/index.php
+++ 01/index.php
@@ -42,11 +42,63 @@
     $texto_sem_espacos = trim($texto);
     echo strlen($texto_sem_espacos);
     echo "<br>";
-    
+
     #strpos - ?
     $lista_alunos = "João, Maria, Fernando, Flor, Maria";
     $posicao_maria = strpos($lista_alunos, "Maria");
     echo "A posição de maria é: $posicao_maria";
+    echo "<br>";
+
+    # strrev(): Inventer uma string
+    $texto = "Socorram me subi no onibus em marrocos";
+    $testo_invertido = strrev($texto);
+    echo $texto_invertido;
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+  
+    # 1
+    // Crie uma variável para nome e outra para profissão
+    // Escreva a mensagem "Fulano é tal_profissao"
+
+    $nome = "Valdir";
+    $sobrenome = "Fernandes";
+    $profissao = "Soldador";
+    echo $nome . " " . $sobrenome . " é " . $profissao;
+    // ou $mensagem = $nome . " é "  . $profissao;
+    //echo $mensagem;
+    echo "<br>";
+    # 2
+    // Crie uma variável senha e aramezene "senha123"
+    // Verifique se a senha tem mais de 8 caracteres
+    // Se sim, diga que a senha é válida. Se não, diga que é inválida
+   
+    $senha = "senha1234";
+        if (strlen($senha) > 8) {
+            echo "A senha é válida.";
+        } else {
+            echo "A senha é inválida.";
+}
+    echo "<br>";
+
+    # 3
+    // Corrija a palavra "ExEmPlO" para que ela fique totalmente em minúsculo
+    echo $exmplo = "ExEmPlO";
+    $exmplo_minusculo = strtolower($exmplo);
+    echo $exmplo_maiusculo;
+
+    echo "<br>";
+   
+
+
+
+    # 4
+    // Corrija o link "  http://exemplo.com  " para que ele não tenha espaços inválidos
+    echo "<br>";
+    # 5
+    // Inverta a string "abcd123" e encontre a posição do caractere "3" após a inversão
+
+
 
 ?>
 
