c2700c98d0254f0c6448864bb539e8a2453209b9
tamarareginafumeiro <tamara.rfumeiro@senacsp.edu.br>
2024-08-07 22:07:50 -0300

    sql

---
+++ anotacoes.sql
@@ -0,0 +1,16 @@
+
+
+use nome do banco; // selecinar tabela
+SELECT * FROM nome da "tabela" // verficar se a tabela foi criada
+
+
+Insert into `nome da tabela` (//nome das colunas 'exemplo', 'exemplo2') VALUES
+(//dados a ser inserido 'exemplo', 'exemplo2')
+
+SELECT * FROM senac_cliente.tb_clientes; #selecionar toda a tabela.
+SELECT * FROM tb_clientes; #selecionar toda a tabela.
+SELECT * FROM tb_clientes where id_cliente = 5; #localizar um iten na tabela.
+Select nome_cliente from tb_clientes; # para localizar uma coluna.
+Select * from tb_clientes order by nome_cliente asc; # para localizar uma coluna.
+
+
---
+++ aula_07_08_24.sql
@@ -0,0 +1,33 @@
+--Selecionando uma tabela
+SELECT * FROM telefones;
+SELECT * FROM amigos;
+
+-- Selecionando uma tuplada tabela (tupla=linha)
+SELECT * FROM telefones where id = 5;
+
+Select amigos.nome, telefones.id_amigo from amigos, telefones where amigos.id = telefones.id_amigo and telefones.id_amigo = 5;
+
+-- Selecionando apenas o telefone da cristina
+Select amigos.nome, telefones.numero from amigos, telefones where amigos.id = telefones.id_amigo and amigos.nome = "Cristina";
+
+-- Quero a mesma query, mas com o nome da coluna igual ao nome da cristina
+Select telefones.numero 'Cristina' from amigos, telefones where amigos.id = telefones.id_amigo and amigos.nome;
+
+--Selecionando uma coluna da tabela:
+SELECT nome from amigos;
+
+--obter dados dos clientes ordenados por ordem alfabetica do nome
+SELECT * FROM amigos order by nome asc;
+SELECT nome FROM amigos order by nome asc;
+
+--Quero todos os numeros que comecem com 5
+Select * from telefones where numero like '5%'
+Select * from amigos where nome like 'd%'
+
+-- Selecionando dois atributos de uma mesma tabela
+SELECT id, nome from amigos;
+
+-- Fazendo uma conta de multiplicação
+SELECT 10*5;
+
+
